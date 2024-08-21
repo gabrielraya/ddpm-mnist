f1c3c00d9dd151b1dd890e1a7c5c2b2fc73eeda7
gaboraya@gmail.com <gaboraya@gmail.com>
2024-08-21 11:06:04 +0200

    add code to filtered blond people for celeba experiment

--- datasets.py
+++ datasets.py
@@ -61,7 +61,9 @@ class FilteredDataset(Dataset):
         return self.dataset[self.indices[idx]]
 
 
-def load_data(config=None, data_path="../datasets", num_workers=4, evaluation=False, distributed=True, target_class=None):
+
+def load_data(config=None, data_path="../datasets", num_workers=4, evaluation=False, distributed=True,
+              target_class=None, selected_attributes=None):
     """Load dataset based on the configuration."""
     # Get transformation functions
     trans, trans_eval = get_transform(config.data.dataset, config.data.image_size, config.data.random_flip)
@@ -70,17 +72,30 @@ def load_data(config=None, data_path="../datasets", num_workers=4, evaluation=Fa
     dataset_cls = getattr(datasets, config.data.dataset)
 
     # Load dataset with given transformations
-    if config.data.dataset == "CelebA":
-        train_dataset = dataset_cls(root=data_path, transform=trans, split='train', download=True)
-        test_dataset = dataset_cls(root=data_path, transform=trans_eval, split='valid', download=True)
+    if config.data.dataset == "CelebA" :
+        train_dataset = dataset_cls(root=data_path, transform=trans, split='train', target_type='attr', download=True)
+        test_dataset = dataset_cls(root=data_path, transform=trans_eval, split='valid', target_type='attr', download=True)
+
+        if selected_attributes:
+            # Step 2: Get the indices of the selected attributes
+            attr_indices = [train_dataset.attr_names.index(attr) for attr in selected_attributes]
+            attr_indices_test = [test_dataset.attr_names.index(attr) for attr in selected_attributes]
+
+            # Step 3: Filter the dataset to include only people with the selected attributee
+            selected_indices = [i for i, attr in enumerate(train_dataset.attr) if attr[attr_indices] == 1]
+            train_dataset = torch.utils.data.Subset(train_dataset, selected_indices)
+
+            selected_indices = [i for i, attr in enumerate(test_dataset.attr) if attr[attr_indices_test] == 1]
+            test_dataset = torch.utils.data.Subset(test_dataset, selected_indices)
+
     else:
         train_dataset = dataset_cls(root=data_path, transform=trans, train=True, download=True)
         test_dataset = dataset_cls(root=data_path, transform=trans_eval, train=False, download=True)
 
-    # Apply filtering by class if target_class is specified
-    if target_class is not None:
-        train_dataset = FilteredDataset(train_dataset, target_class)
-        test_dataset = FilteredDataset(test_dataset, target_class)
+        # Apply filtering by class if target_class is specified
+        if target_class is not None:
+            train_dataset = FilteredDataset(train_dataset, target_class)
+            test_dataset = FilteredDataset(test_dataset, target_class)
 
     # Setup samplers
     train_sampler = DistributedSampler(train_dataset) if distributed else None
